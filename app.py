d730063a149dda8b80473430a0f6bd040c68474d
Samuel Hartmann <hartmann.samuel@gmx.de>
2023-06-19 15:02:35 +0200

    implement upload of multiple files

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ ALLOWED_EXTENSIONS = {'pdf', 'tif', 'tiff'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'super secret key'
+app.debug = True
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -42,6 +46,41 @@ def upload_file():
       <input type=submit value=Upload>
     </form>
     '''
+
+@app.route('/upload-multiple', methods=['GET', 'POST'])
+def upload_multiple():  # flask upload multiple files: https://stackoverflow.com/a/11817318/12946000 & https://youtu.be/krcyh42ShLg
+    print(f'request.files: {request.files}')
+
+    upload_files = request.files.getlist("file")
+    print(f'upload_files: {upload_files}')
+
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        for file in upload_files:
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                # return redirect(url_for('download_file', name=filename))    # flask url_for(): https://stackoverflow.com/a/7478705/12946000
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file multiple>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
 
 # download uploaded file as is
 @app.route('/uploads/<name>')
